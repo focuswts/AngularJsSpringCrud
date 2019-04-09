996acc3c8cd11f402e3c75b24c63edd35bf05141
1676886 <1676886@07HW012072.SOAM.TCS.com>
2019-04-09 15:49:23 -0300

    Primeiro Commit

---
+++ angularjsSpringCrud/src/main/java/com/angularjs/crud/angularjsSpringCrud/AngularjsSpringCrudApplication.java
@@ -0,0 +1,13 @@
+package com.angularjs.crud.angularjsSpringCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+ 
+@SpringBootApplication
+public class AngularjsSpringCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularjsSpringCrudApplication.class, args);
+	}
+
+}
---
+++ angularjsSpringCrud/src/main/java/com/angularjs/crud/angularjsSpringCrud/controller/AppController.java
@@ -0,0 +1,15 @@
+package com.angularjs.crud.angularjsSpringCrud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AppController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+
+	}
+
+}
